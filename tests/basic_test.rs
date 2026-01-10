3b304c3ff5e29db0e88651550969ae6b07377ba8
fb <fb@dev.null>
2026-01-10 12:54:53 +0100

    move main to tests

--- tests/basic_test.rs
+++ tests/basic_test.rs
@@ -1,5 +1,5 @@
 use observlib::KeyValue;
-pub fn main() {
+pub fn basic_instantiation() {
     let attrs = vec![KeyValue::new("env", "dev")];
     let otel_manager = observlib::initialize_telemetry("blah", "127.0.0.1:4318", attrs);
     otel_manager.shutdown().unwrap();
