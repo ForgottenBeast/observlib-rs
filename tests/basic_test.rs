6e458c8e3d25cae7d9beefa6d9d82c12753193d3
fb <fb@dev.null>
2026-01-10 16:19:20 +0100

    test counter creation

--- tests/basic_test.rs
+++ tests/basic_test.rs
@@ -1,6 +1,11 @@
 use observlib::KeyValue;
+use opentelemetry::global;
+
+#[test]
 pub fn basic_instantiation() {
     let attrs = vec![KeyValue::new("env", "dev")];
     let otel_manager = observlib::initialize_telemetry("blah", "127.0.0.1:4318", attrs);
+    let counter = global::meter("my meter").u64_counter("my_counter").build();
+    counter.add(1,&[]);
     otel_manager.shutdown().unwrap();
 }
